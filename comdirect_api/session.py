ebcf7a9a6407aa11a12575b59db6584c9ae50a42
Caetano Sauer <caetano.sauer@salesforce.com>
2024-05-19 09:07:50 +0200

    Simplify API and implementa actual refresh call
    - Session.refresh() was actually a revoke call. I implemented the actual
      refresh here
    - Removed all Account-related classes, since we can just use the JSON
      objects
    - Removed autorefresh functionality

--- comdirect_api/session.py
+++ comdirect_api/session.py
@@ -2,10 +2,8 @@ import requests
 import uuid
 import base64
 import json
-import time
-import threading
 
-import comdirect_api.types
+from comdirect_api.types import Depot, DepotBalance, Document
 from comdirect_api.utils import default_callback_p_tan, \
     default_callback_m_tan,\
     default_callback_p_tan_push,\
@@ -21,11 +19,8 @@ class Session:
         client_secret,
         callback_p_tan=default_callback_p_tan,
         callback_m_tan=default_callback_m_tan,
-        callback_p_tan_push=default_callback_p_tan_push,
-        autorefresh=False,
+        callback_p_tan_push=default_callback_p_tan_push
     ):
-        self.autorefresh = autorefresh
-
         # POST /oauth/token
         response = requests.post(
             "https://api.comdirect.de/oauth/token",
@@ -164,17 +159,6 @@ class Session:
         self.kontakt_id = tmp["kontaktId"]  # no clue what this is
         self.isRevoked = False
 
-        if self.autorefresh:
-            self.refreshThread = threading.Thread(target=self._refresh_worker)
-            self.refreshThread.start()
-
-    def _refresh_worker(self):
-        while not self.isRevoked:
-            time.sleep(9 * 60)
-            if self.isRevoked:
-                break
-            else:
-                self.refresh()
 
     def revoke(self):
         if not self.isRevoked:
@@ -195,23 +179,27 @@ class Session:
                 )
             self.isRevoked = True
 
-    def refresh(self):
-        # https://api.comdirect.de/oauth/revoke
-        response = requests.delete(
-            "https://api.comdirect.de/oauth/revoke",
+
+    def refresh(self, client_id, client_secret):
+        # POST /oauth/token
+        response = requests.post(
+            "https://api.comdirect.de/oauth/token",
             allow_redirects=False,
             headers={
                 "Accept": "application/json",
-                "Authorization": f"Bearer {self.access_token}",
                 "Content-Type": "application/x-www-form-urlencoded",
             },
-            data=f'{{"identifier":"{self.session_id}","sessionTanActive":true,"activated2FA":true}}',
+            data=f"client_id={client_id}&client_secret={client_secret}&"
+                 f"grant_type=cd_secondary&token={self.access_token}",
         )
-        if not response.status_code == 204:
+        if not response.status_code == 200:
             raise RuntimeError(
-                f"DELETE https://api.comdirect.de/oauth/revoke returned unexpected status {response.status_code}"
+                f"POST https://api.comdirect.de/oauth/token returned unexpected status {response.status_code}: {response.json()}"
             )
-        self.isRevoked = True
+        tmp = response.json()
+        self.access_token = tmp["access_token"]
+        self.refresh_token = tmp["refresh_token"]
+
 
     def _get_authorized(self, url, extraheaders={}):
         headers = {
@@ -232,42 +220,49 @@ class Session:
         response = self._get_authorized(
             f"https://api.comdirect.de/api/banking/clients/user/v1/accounts/balances"
             f"?paging-first={paging_first}&paging-count={paging_count}")
-        return [comdirect_api.types.AccountBalance(i) for i in response.json()["values"]]
+        for v in response.json()["values"]:
+            yield v
 
     # GET /banking/v2/accounts/{accountid}/balances
     def account_get_balance(self, accountid):
         response = self._get_authorized(f"https://api.comdirect.de/api/banking/v2/accounts/{accountid}/balances")
-        return comdirect_api.types.AccountBalance(response.json())
+        return response.json()
 
     # GET /banking/v2/accounts/{accountid}/transactions
     # TODO: The filters do not yet work.
     # see https://community.comdirect.de/t5/Website-Apps/Rest-API-Filter-Parameter/m-p/108710
     def account_get_transactions(self, accountid, transactionDirection="CREDIT_AND_DEBIT",
-                                 min_bookingdate=None, max_bookingdate=None, transactionState="BOTH"):
+                                 min_bookingdate=None, max_bookingdate=None, transactionState="BOOKED"):
         parameters = f"transactionDirection={transactionDirection}&transactionState={transactionState}"
         if min_bookingdate:
             parameters += f"&min-bookingdate={min_bookingdate}"
         if max_bookingdate:
             parameters += f"&min-bookingdate={max_bookingdate}"
-        response = self._get_authorized(
-            f"https://api.comdirect.de/api/banking/v1/accounts/{accountid}/transactions?{parameters}")
-        return [comdirect_api.types.AccountTransaction(i) for i in response.json()["values"]]
+        index = 0
+        while True:
+            response = self._get_authorized(
+                f"https://api.comdirect.de/api/banking/v1/accounts/{accountid}/transactions?{parameters}&paging-first={index}&paging-count=100")
+            count = response.json()["paging"]["matches"]
+            for v in response.json()["values"]:
+                yield v
+            index += len(response.json()["values"])
+            if index >= count:
+                break
 
     # --------------------------- DEPOT ----------------------------------------
-
     # GET /brokerage/clients/clientId/v3/depots
 
     def account_get_depots(self, paging_first=0, paging_count=1000):
         response = self._get_authorized(
             f"https://api.comdirect.de/api/brokerage/clients/user/v3/depots"
             f"?paging-first={paging_first}&paging-count={paging_count}")
-        return [types.Depot(i) for i in response.json()["values"]]
+        return [Depot(i) for i in response.json()["values"]]
 
     # GET /brokerage/v3/depots/{depotId}/positions
     def account_get_depot_positions(self, depotid):
         response = self._get_authorized(
             f"https://api.comdirect.de/api/brokerage/v3/depots/{depotid}/positions")
-        return types.DepotBalance(response.json()["aggregated"])
+        return DepotBalance(response.json()["aggregated"])
 
     # ------------------------------ DOCUMENTS --------------------------------
     # GET /messages/clients/user/v2/documents
@@ -277,7 +272,7 @@ class Session:
         response = self._get_authorized(
             f"https://api.comdirect.de/api/messages/clients/{uuid}/v2/documents?"
             f"paging-first={paging_first}&paging-count={paging_count}")
-        return [comdirect_api.types.Document(i) for i in response.json()["values"]]
+        return [Document(i) for i in response.json()["values"]]
 
     # GET /messages/v2/documents/{documentId}
     def documents_download(self, document):
--- comdirect_api/types.py
+++ comdirect_api/types.py
@@ -1,6 +1,5 @@
 import datetime
 import comdirect_api.utils
-from decimal import Decimal
 
 
 class DateString():
@@ -28,6 +27,8 @@ AccountType = {
 # TODO: This list is incomplete.
 TransactionType = {
     "ATM_WITHDRAWAL": "ATM Withdrawal",
+    "BANK_FEES": "Bank Fees",
+    "CARD_TRANSACTION": "Card Transaction",
     "DIRECT_DEBIT": "Direct Debit",
     "SECURITIES": "Securities",
     "TRANSFER": "Transfer",
@@ -47,121 +48,6 @@ TransactionType = {
     "XXC": "Savings",
     "XXD": "Standing Order",
 }
-
-
-class AmountValue():
-    """ create from JSON REST response
-
-    Example input will look like the following:
-    {'unit':'EUR', 'value': '123.45'}
-    """
-
-    def __init__(self, json):
-        self.unit = json["unit"]
-        self.value = Decimal(json["value"])
-
-    def get_amount(self):
-        return self.value
-
-    def __str__(self):
-        return f"{self.value} {self.unit}"
-
-
-class Account():
-    """ create from JSON REST response
-
-    Example input will look like the following:
-        {'accountDisplayId': '12345678901',
-        'accountId': '148B8582CFB8F2C0CB8D3E56131891E8',
-        'accountType': {'key': 'DAS', 'text': 'Tagesgeld PLUS-Konto'},
-        'clientId': 'C87A5216C263B8367CAAE2622EB571F9',
-        'creditLimit': {'unit': 'EUR', 'value': '0'},
-        'currency': 'EUR',
-        'iban': 'DE0820041112345678901'}
-    """
-
-    def __init__(self, json):
-        self.accountDisplayId = json["accountDisplayId"]
-        self.accountId = json["accountId"]
-        self.accountType = json["accountType"]["key"]
-        assert(self.accountType in AccountType)
-        self.clientId = json["clientId"]
-        self.creditLimit = AmountValue(json["creditLimit"])
-        self.currency = json["currency"]
-        self.iban = json["iban"]
-
-    def get_accountId(self):
-        return self.accountId
-
-    def __str__(self):
-        return f"{AccountType[self.accountType]} ({self.iban})"
-
-
-class AccountBalance():
-    """ create from JSON REST response """
-
-    def __init__(self, json):
-        if 'account' in json:
-            self.account = Account(json["account"])
-        self.accountId = json["accountId"]
-        self.availableCashAmount = AmountValue(json["availableCashAmount"])
-        self.availableCashAmountEUR = AmountValue(json["availableCashAmountEUR"])
-        self.balance = AmountValue(json["balance"])
-        self.balanceEUR = AmountValue(json["balanceEUR"])
-        assert(self.balanceEUR.unit == "EUR")
-        assert(self.availableCashAmountEUR.unit == "EUR")
-
-    def __str__(self):
-        return str(self.balance)
-
-
-class AccountTransaction():
-    """ create from JSON REST response
-
-    Example input will look like the following:
-        {'amount': {'unit': 'EUR', 'value': '-12.34'},
-        'bookingDate': '2020-01-01',
-        'bookingStatus': 'BOOKED',
-        'creditor': None,
-        'deptor': None,
-        'directDebitCreditorId': None,
-        'directDebitMandateId': None,
-        'endToEndReference': None,
-        'newTransaction': True,
-        'reference': '50C12345A1234567/12345',
-        'remittanceInfo': '01Globus TS Forchheim//Forchheim/DE  '
-                          '022020-01-01T20:07:16 KFN 0  VJ 1234',
-        'remitter': {'holderName': 'Globus Handelshof'},
-        'transactionType': {'key': 'DIRECT_DEBIT', 'text': 'Direct Debit'},
-        'valutaDate': '2020-01-03'},
-
-    """
-
-    def __init__(self, json):
-        self.amount = AmountValue(json["amount"])
-        self.bookingDate = DateString(json["bookingDate"]) if json["bookingDate"] else None
-        self.bookingStatus = json["bookingStatus"]
-        assert(self.bookingStatus in ["BOOKED", "NOTBOOKED"])
-        self.booked = (self.bookingStatus == "BOOKED")
-        self.creditor = json["creditor"]
-        self.deptor = json["deptor"]
-        self.directDebitCreditorId = json["directDebitCreditorId"]
-        self.directDebitMandateId = json["directDebitMandateId"]
-        self.endToEndReference = json["endToEndReference"]
-        self.newTransaction = json["newTransaction"]
-        self.reference = json["reference"]
-        self.remittanceInfo = json["remittanceInfo"]
-        self.remitter = json["remitter"]
-        self.transactionType = json["transactionType"]["key"]
-        # TODO: This will break if you have other transaction types.
-        # The problem is that comdirect didn't document the keys.
-        # Please report, if you learn what keys there are!
-        print(self.transactionType)
-        assert(self.transactionType in TransactionType)
-        self.valutaDate = json["valutaDate"]  # can be an invalid date!
-
-    def __str__(self):
-        return f"{self.bookingDate} {self.amount} {self.transactionType}"
 
 
 # --------------------------- DEPOT ----------------------------------------
